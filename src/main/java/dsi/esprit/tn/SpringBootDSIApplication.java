ddb4d194ddf7eb1627656f3cf92e75719505d8ad
OussamaLhl <oussama.lahouel@esprit.tn>
2023-08-22 16:26:12 +0100

    1st commit-Authentication

---
+++ src/main/java/dsi/esprit/tn/SpringBootDSIApplication.java
@@ -0,0 +1,10 @@
+package dsi.esprit.tn;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDSIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDSIApplication.class, args);
+    }
+}
