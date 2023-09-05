682f89c53da119b78e75914eee0f5b6c65708d32
OussamaLhl <oussama.lahouel@esprit.tn>
2023-09-05 14:16:41 +0100

    SignUp/SignIn with MySQLDB

--- src/main/java/dsi/esprit/tn/Models/ERole.java
+++ src/main/java/dsi/esprit/tn/Models/ERole.java
@@ -1,7 +1,7 @@
-package dsi.esprit.tn.Models;
+package dsi.esprit.tn.models;
 
 public enum ERole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN
 }
