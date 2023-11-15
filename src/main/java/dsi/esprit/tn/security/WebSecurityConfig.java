7daddbf568a8e6824e939503a67f92bd89289e37
OussamaLhl <oussama.lahouel@esprit.tn>
2023-11-15 15:39:14 +0100

    containerization

--- src/main/java/dsi/esprit/tn/security/WebSecurityConfig.java
+++ src/main/java/dsi/esprit/tn/security/WebSecurityConfig.java
@@ -102,7 +102,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth ->
           auth.antMatchers("/api/auth/**").permitAll()
-              .antMatchers("/api/test/**").permitAll()
+              .antMatchers("/api/test/**").permitAll().antMatchers("/authentication-ms/api/**").permitAll()
               .anyRequest().authenticated()
         );
 
